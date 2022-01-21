91e322fb8cd29e42ac3ffe09ba7c7d4d0c24baa6
vitkolos <vit.kolos@gmail.com>
2022-01-21 08:16:35 +0100

    fixed wrap

--- script.js
+++ script.js
@@ -21,18 +21,18 @@ const formatFunctions = [
         return getPartOfSuplovani(2)
     },
     function statusbar() {
-        const wrap = (html) => `<span>${html}</span>`
+        const wrap = (html) => (html ? `<span>${html}</span>` : '')
         let logo = '<img src="goh.svg" />'
         let suplovaniDate = (
             (getPartOfSuplovani(0) !== getPartOfSuplovani(1))
-                ? wrap(`zobrazuje se suplování pro ${getPartOfSuplovani(1)}`) : ''
+                ? `zobrazuje se suplování pro ${getPartOfSuplovani(1)}` : ''
         )
         return (
             logo
             + wrap(getPartOfSuplovani(0))
-            + (gec('owm') ? wrap(gec('owm')) : '')
+            + wrap(gec('owm'))
             + wrap(gec('nameday'))
-            + suplovaniDate
+            + wrap(suplovaniDate)
         )
     }
 ]
